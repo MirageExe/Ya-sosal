6ff09923790d444d715b0785c5bca40b095d09f5
NEirotoXiN00 <yarikk5865@gmail.com>
2026-05-16 17:17:02 +0300

    Anomaly Event

---
+++ Content.Server/_Rat/SpaceEvents/AnomalyStationRuleComponent.cs
@@ -0,0 +1,27 @@
+using System.Numerics;
+using Content.Server._Rat.SpaceEvents;
+using Robust.Shared.Serialization.TypeSerializers.Implementations.Custom.Prototype;
+using Robust.Shared.Prototypes;
+
+namespace Content.Server._Rat.SpaceEvents.Components;
+
+[RegisterComponent, Access(typeof(AnomalyStationRule))]
+public sealed partial class AnomalyStationRuleComponent : Component
+{
+    [DataField]
+    public float MinCoord = -3000f;
+
+    [DataField]
+    public float MaxCoord = 3000f;
+
+    [DataField(required: true)]
+    public string StationMapPath;
+
+    [DataField(required: true)]
+    public LocId Announcement;
+
+    [DataField("anomalySpawnerPrototype", customTypeSerializer: typeof(PrototypeIdSerializer<EntityPrototype>))]
+    public string AnomalySpawnerPrototype = "RandomAnomalySpawner";
+
+    public Vector2 Coordinates;
+}
